dda06d36431ad1699233f89842f6dfc742f960e3
hej566 <hejin.udem@gmail.com>
2022-01-16 21:03:27 -0500

    [refactor] deep copy function

---
+++ src/utils/deepClone.js
@@ -0,0 +1,22 @@
+const deepClone = (target) => {
+  if (target === null) {
+    return target;
+  }
+  if (Array.isArray(target)) {
+    const copy = [];
+    target.forEach((item) => {
+      copy.push(deepClone(item));
+    });
+    return copy;
+  }
+  if (typeof target === 'object') {
+    const copy = {};
+    target.forEach((item) => {
+      copy[item] = deepClone(target[item]);
+    });
+    return copy;
+  }
+  return target;
+};
+
+export default deepClone;
